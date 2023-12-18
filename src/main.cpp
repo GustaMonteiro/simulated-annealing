80eccc4e447439c08bd0ae89a9957d63a0a35365
GustaMonteiro <gcvm6070@gmail.com>
2023-12-17 22:25:26 -0300

    Add comments to matching.cpp and main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,39 +12,52 @@
 #include "resultData.hpp"
 #include "logger.hpp"
 
+// Map of input modes and corresponding functions for handling input
 std::map<std::string, std::function<void(int, std::vector<std::vector<double>> &, std::istream &)>> inputModes = {
     {"--matrix", matrixModeInput},
     {"--edge", edgeModeInput},
     {"--weight", weightsModeInput},
     {"--coordinates", coordinatesModeInput}};
 
+// Set of accepted input modes
 std::set<std::string> acceptedModes{"--manual", "--matrix", "--edge", "--weight", "--coordinates", "--file"};
 
+// Map to convert mode numbers to mode strings
 std::map<int, std::string> modeNumberToString = {
     {0, "--matrix"},
     {1, "--edge"},
     {2, "--weight"},
     {3, "--coordinates"}};
 
+// Main function
 int main(int argc, char **argv)
 {
+  // Display usage information if no arguments or help flag is provided
   if (argc < 2 || (argc == 2 && (std::string(argv[1]) == "--help" || std::string(argv[1]) == "-h")))
     printUsage(argv[0]);
 
+  // Get the input mode from the command line arguments
   std::string mode(argv[1]);
+
+  // Check if the provided input mode is accepted
   if (acceptedModes.find(mode) == acceptedModes.end())
     printUsage(argv[0]);
 
+  // If manual mode is selected, prompt the user for input
   if (mode == "--manual")
     getInputModeManually(mode);
 
+  // If file mode is selected
   if (mode == "--file")
   {
+    // Check if a file path is provided
     if (argc < 3)
       printUsage(argv[0]);
 
+    // Get the file path from the command line arguments
     std::filesystem::path filePath = argv[2];
 
+    // Open the file
     std::ifstream file(filePath);
     if (!file.is_open())
     {
@@ -52,6 +65,7 @@ int main(int argc, char **argv)
       exit(-1);
     }
 
+    // Read the input mode and number of vertices from the file
     int m;
     file >> m;
     if (m < 0 || m > 3)
@@ -61,32 +75,42 @@ int main(int argc, char **argv)
       exit(-1);
     }
 
+    // Convert mode number to mode string
     mode = modeNumberToString[m];
     int n;
     file >> n;
 
+    // Create a matrix to store weights
     std::vector<std::vector<double>> weights(n, std::vector<double>(n));
 
+    // Call the corresponding input mode function to read data from the file
     inputModes[mode](n, weights, file);
 
+    // Close the file
     file.close();
 
+    // Perform simulated annealing and print result information
     ResultData result = simulatedAnnealing(weights, 6000, filePath, mode);
     result.printInfos();
 
+    // Log the result
     Logger::log(result);
 
     return 0;
   }
 
+  // If not in file mode, prompt the user for the number of vertices
   int n;
   std::cout << "Insert the number of vertices (n): ";
   std::cin >> n;
 
+  // Create a matrix to store weights
   std::vector<std::vector<double>> weights(n, std::vector<double>(n));
 
+  // Call the corresponding input mode function to read data from the user
   inputModes[mode](n, weights, std::cin);
 
+  // Perform simulated annealing and print result information
   ResultData result = simulatedAnnealing(weights);
   result.printInfos();
 
--- src/matching.cpp
+++ src/matching.cpp
@@ -14,6 +14,7 @@
 #include "defines.hpp"
 #include "resultData.hpp"
 
+// Function to calculate the cost of a permutation and check if it's a valid matching
 std::pair<double, bool> getPermutationCost(const std::vector<int> &permutation, const std::vector<std::vector<double>> &weights)
 {
   double cost = 0;
@@ -32,64 +33,77 @@ std::pair<double, bool> getPermutationCost(const std::vector<int> &permutation, 
   return {cost, validMatching};
 }
 
+// Simulated Annealing algorithm for finding an optimal matching
 ResultData simulatedAnnealing(const std::vector<std::vector<double>> &weights, int numIterations, std::string instancePath, std::string inputMode)
 {
   ResultData result;
 
+  // Parameters for Simulated Annealing
   double Tm = 0.0001;
   double T0 = 100;
   double alpha = 0.95;
+
+  // Seed for random number generation
   std::srand(std::time(0));
+
+  // Number of vertices in the graph
   int n = weights.size();
 
+  // Check if the number of vertices is odd
   bool nIsOdd = false;
-
   if (n % 2 != 0)
     nIsOdd = true;
 
+  // Initialize the current best permutation randomly
   std::vector<int> currentBestPermutation(n);
   std::iota(currentBestPermutation.begin(), currentBestPermutation.end(), 0);
-
   std::random_device rd;
   std::mt19937 g(rd());
-
   std::shuffle(currentBestPermutation.begin(), currentBestPermutation.end(), g);
 
+  // Initialize the best permutation and its cost
   std::vector<int> bestPermutation = currentBestPermutation;
-
   double bestPermutationCost = getPermutationCost(bestPermutation, weights).first;
   double currentBestPermutationCost = bestPermutationCost;
 
+  // Initialize the ResultData structure
   result.instancePath = instancePath;
   result.inputMode = inputMode;
   result.allCalculatedCosts.push_back(bestPermutationCost);
   result.firstMatchingCost = bestPermutationCost;
   result.numberOfVertices = n;
 
+  // Initialize temperature
   double T = T0;
 
+  // Start the timer
   auto start = std::chrono::high_resolution_clock::now();
 
+  // Simulated Annealing loop
   while (T > Tm)
   {
     for (int i = 0; i < numIterations; i++)
     {
+      // Generate a neighboring permutation by swapping two elements
       auto neighbor = currentBestPermutation;
       int l = std::rand() % n;
       int r = std::rand() % n;
-
       std::iter_swap(neighbor.begin() + l, neighbor.begin() + r);
 
+      // Calculate the cost of the neighboring permutation
       double neighborCost = getPermutationCost(neighbor, weights).first;
       result.allCalculatedCosts.push_back(neighborCost);
 
+      // Calculate the change in cost
       double delta = neighborCost - currentBestPermutationCost;
 
+      // Accept the neighboring permutation if it improves the cost
       if (delta < 0)
       {
         currentBestPermutation = neighbor;
         currentBestPermutationCost = neighborCost;
 
+        // Update the best permutation if needed
         if (neighborCost < bestPermutationCost)
         {
           bestPermutation = neighbor;
@@ -98,6 +112,7 @@ ResultData simulatedAnnealing(const std::vector<std::vector<double>> &weights, i
       }
       else
       {
+        // Accept the neighboring permutation with a certain probability
         double exponent = ((double)-delta) / T;
         double prob = std::exp(exponent);
         double randomValue = (double)rand() / RAND_MAX;
@@ -109,25 +124,29 @@ ResultData simulatedAnnealing(const std::vector<std::vector<double>> &weights, i
         }
       }
     }
+    // Reduce temperature
     T *= alpha;
   }
 
+  // If the number of vertices is odd, remove the last vertex from the best permutation
   if (nIsOdd)
     bestPermutation.pop_back();
 
+  // Stop the timer
   auto end = std::chrono::high_resolution_clock::now();
 
+  // Calculate execution time
   std::chrono::duration<double> duration = end - start;
-
   result.executionTime = duration.count();
   result.bestMatching = bestPermutation;
   result.bestMatchingCost = bestPermutationCost;
 
+  // Calculate the mean of all calculated costs
   double sum = 0.0;
   for (auto cost : result.allCalculatedCosts)
     sum += cost;
-
   result.calculatedCostsMean = sum / result.allCalculatedCosts.size();
 
+  // Return the ResultData structure
   return result;
 }
